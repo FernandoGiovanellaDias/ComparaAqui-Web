381b6119b2af6b7c7dc1e8c1b44a558241785775
DESKTOP-NP256PN\Pichau <fagdias2002@hotmail.com>
2024-11-02 10:08:25 -0300

    correção das funções

--- migrations/20241027192139-functions.js
+++ migrations/20241027192139-functions.js
@@ -3,69 +3,162 @@
 module.exports = {
     up: async (queryInterface, Sequelize) => {
       await queryInterface.sequelize.query(`
-            DROP FUNCTION IF EXISTS buscar_mercados_com_produtos;
-            CREATE OR REPLACE FUNCTION public.buscar_mercados_com_produtos(IN idsprod integer[])
-            RETURNS TABLE(id_mercado integer, name text, ordem integer, totalitems integer, totalvalue double precision, produtos_ausentes text[])
-            LANGUAGE 'plpgsql'
+            DROP FUNCTION IF EXISTS public.array_diff(text[], text[]);
+
+            CREATE OR REPLACE FUNCTION public.array_diff(
+                arr1 text[],
+                arr2 text[])
+                RETURNS text[]
+                LANGUAGE 'plpgsql'
+                COST 100
+                VOLATILE PARALLEL UNSAFE
+            AS $BODY$
+            DECLARE
+                result TEXT[];
+            BEGIN
+                result := ARRAY(
+                    SELECT unnest(arr1)
+                    EXCEPT
+                    SELECT unnest(arr2)
+                );
+                RETURN result;
+            END;
+            $BODY$;
+
+            ALTER FUNCTION public.array_diff(text[], text[])
+                OWNER TO postgres;
+
+            DROP FUNCTION IF EXISTS public.calcular_classificacao(integer, integer);
+
+            CREATE OR REPLACE FUNCTION public.calcular_classificacao(
+                totalitems integer,
+                tamanho_idsprod integer)
+                RETURNS text
+                LANGUAGE 'plpgsql'
+                COST 100
+                VOLATILE PARALLEL UNSAFE
+            AS $BODY$
+            BEGIN
+                IF totalitems < 4 THEN
+                    RETURN CASE 
+                            WHEN totalitems = tamanho_idsprod THEN 'RECOMENDADO'
+                            WHEN (tamanho_idsprod - totalitems) < 2 THEN 'PARCIAL'
+                            ELSE 'GERAL'
+                        END;
+                ELSE
+                    RETURN CASE 
+                            WHEN totalitems = tamanho_idsprod THEN 'RECOMENDADO'
+                            WHEN (tamanho_idsprod - totalitems) < 4 THEN 'PARCIAL'
+                            ELSE 'GERAL'
+                        END;
+                END IF;
+            END;
+            $BODY$;
+
+            ALTER FUNCTION public.calcular_classificacao(integer, integer)
+                OWNER TO postgres;
+
+            DROP FUNCTION IF EXISTS public.obter_padroes_nome(integer[]);
+
+            CREATE OR REPLACE FUNCTION public.obter_padroes_nome(
+                idsprod integer[])
+                RETURNS text[]
+                LANGUAGE 'plpgsql'
+                COST 100
+                VOLATILE PARALLEL UNSAFE
             AS $BODY$
             DECLARE
                 nomesProdutos text[];
+            BEGIN
+                nomesProdutos := (SELECT array_agg(distinct '%' || p.name || '%') FROM produto p WHERE p.id = ANY(idsprod));
+                RETURN nomesProdutos;
+            END;
+            $BODY$;
+
+            ALTER FUNCTION public.obter_padroes_nome(integer[])
+                OWNER TO postgres;
+
+            DROP FUNCTION IF EXISTS public.obter_padroes_nome_sem_busca(integer[]);
+
+            CREATE OR REPLACE FUNCTION public.obter_padroes_nome_sem_busca(
+                idsprod integer[])
+                RETURNS text[]
+                LANGUAGE 'plpgsql'
+                COST 100
+                VOLATILE PARALLEL UNSAFE
+            AS $BODY$
+            DECLARE
+                nomesProdutos text[];
+            BEGIN
+                nomesProdutos := (SELECT array_agg(distinct p.name ) FROM produto p WHERE p.id = ANY(idsprod));
+                RETURN nomesProdutos;
+            END;
+            $BODY$;
+
+            ALTER FUNCTION public.obter_padroes_nome_sem_busca(integer[])
+                OWNER TO postgres;
+
+            DROP FUNCTION IF EXISTS public.buscar_mercados_com_produtos(integer[]);
+
+            CREATE OR REPLACE FUNCTION public.buscar_mercados_com_produtos(
+                idsprod integer[])
+                RETURNS TABLE(id_mercado integer, name text, classification text, totalitems integer, totalvalue double precision, itensfaltantes text[]) 
+                LANGUAGE 'plpgsql'
+                COST 100
+                VOLATILE PARALLEL UNSAFE
+                ROWS 1000
+
+            AS $BODY$
+            DECLARE
+                nomesProdutos text[];
+                nomesProdutosSemBusca text[];
                 tamanho_idsprod integer; 
                 retorno record;
             BEGIN
-                -- Chama a função para obter os padrões de nome dos produtos
                 nomesProdutos := obter_padroes_nome(idsprod);
+                nomesProdutosSemBusca := obter_padroes_nome_sem_busca(idsprod);
                 tamanho_idsprod := array_length(nomesProdutos, 1);
 
-                -- Popula retornos com mercados que possuem produtos correspondentes aos padrões ILIKE
                 FOR retorno IN
                     SELECT 
                         p.id_mercado,
                         m.name::text AS name,
                         calcular_classificacao(COUNT(p.id)::integer, tamanho_idsprod) AS classification,
                         COUNT(p.id) AS totalitems,
-                        COALESCE(SUM(p.price), 0) AS totalvalue,
-                  ROW_NUMBER() OVER (ORDER BY COUNT(p.id) DESC) AS row_num
+                        ROUND(COALESCE(SUM(p.price), 0)::numeric, 2)::double precision AS totalvalue,
+                        array_agg(p.name)::text[] as nomesencontrados,
+                        ROW_NUMBER() OVER (ORDER BY COUNT(p.id) DESC, sum(p.price)) AS row_num
                     FROM produto p
                     JOIN mercado m ON m.id = p.id_mercado
                     WHERE p.name ILIKE ANY(nomesProdutos)
                     GROUP BY p.id_mercado, m.name
-                    ORDER BY totalitems DESC
+                    ORDER BY row_num 
                 LOOP
-                    -- Define o array produtos_ausentes
                     RETURN QUERY
                     SELECT 
                         retorno.id_mercado,
                         retorno.name::text,
-                        retorno.row_num::integer AS ordem,
+                        retorno.classification,
                         retorno.totalitems::integer,
                         retorno.totalvalue,
-                        CASE 
-                            WHEN retorno.totalitems = tamanho_idsprod THEN NULL
-                            ELSE ARRAY(
-                                SELECT DISTINCT pr.name::text 
-                                FROM produto pr
-                                WHERE pr.id = ANY(idsprod) 
-                                AND pr.name ILIKE ANY(nomesProdutos)
-                                AND pr.id NOT IN (
-                                    SELECT p.id 
-                                    FROM produto p 
-                                    WHERE p.id_mercado = retorno.id_mercado 
-                                    AND p.name ILIKE ANY(nomesProdutos)
-                                )
-                            )
-                        END AS produtos_ausentes;
+                        array_diff(nomesProdutosSemBusca , retorno.nomesencontrados) AS produtos_ausentes;
                 END LOOP;
             END;
             $BODY$;
 
+            ALTER FUNCTION public.buscar_mercados_com_produtos(integer[])
+                OWNER TO postgres;
 
       `);
     },
 
     down: async (queryInterface, Sequelize) => {
       await queryInterface.sequelize.query(`
-            drop function if exists buscar_mercados_com_produtos;
+            DROP FUNCTION IF EXISTS public.array_diff(text[], text[]);
+            DROP FUNCTION IF EXISTS public.calcular_classificacao(integer, integer);
+            DROP FUNCTION IF EXISTS public.obter_padroes_nome(integer[]);
+            DROP FUNCTION IF EXISTS public.obter_padroes_nome_sem_busca(integer[]);
+            DROP FUNCTION IF EXISTS public.buscar_mercados_com_produtos(integer[]);
       `);
     }
 };
